ffd22940dce63cb94aad52a1bd5461751ac17b46
alexander-iana <alexander.ajay@outlook.com>
2017-05-27 14:51:08 +0530

    Initial commit
    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,8 @@
+
+public class Data {
+	int data[] = {3, 6, 9};
+	
+	public void display() {
+		System.out.println("SSSS S S S S ");
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data d = new Data();
+		display();
+		d.display();
+	}
+	private static void display(){
+		System.out.println("DD");
+	}
+
+}
